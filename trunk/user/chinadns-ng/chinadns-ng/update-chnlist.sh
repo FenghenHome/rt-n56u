42d8a06f092a351d873e00f0a63f32218ab95469
FenghenHome <FenghenHome@users.noreply.github.com>
2021-02-23 18:51:52 +0800

    Update update-chnlist.sh

--- trunk/user/chinadns-ng/chinadns-ng/update-chnlist.sh
+++ trunk/user/chinadns-ng/chinadns-ng/update-chnlist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-url='https://raw.github.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
+url='https://cdn.jsdelivr.net/gh/FenghenHome/filters@gh-pages/dnsmasq.accelerated-domains.conf'
 data=$(curl -4sSkL "$url") || { echo "download failed, exit-code: $?"; exit 1; }
 echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
