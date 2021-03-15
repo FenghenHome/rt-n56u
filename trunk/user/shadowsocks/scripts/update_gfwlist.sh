1b017e66e1ea180c43b58186a07642f0919dbc3e
FenghenHome <s.sunfeihu@gmail.com>
2021-03-15 10:33:15 +0800

    Revert "Update shadowsocks"
    This reverts commit f7ca80ec37ce006fd85302744f58b0d0a1e75ae4.

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -3,14 +3,13 @@
 set -e -o pipefail
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 logger -st "gfwlist" "Starting update..."
-rm -f /tmp/gfwlist_list.conf
 curl -k -s -o /tmp/gfwlist_list.conf --connect-timeout 15 --retry 5 $(nvram get ss_gfwlist_url)
 count=`awk '{print NR}' /tmp/gfwlist_list.conf|tail -n1`
 if [ $count -gt 1000 ]; then
-rm -f /tmp/dnsmasq.dom/gfwlist_list.conf
-cp -r /tmp/gfwlist_list.conf /tmp/dnsmasq.dom/gfwlist_list.conf
+rm -f /etc/storage/gfwlist/gfwlist_list.conf
+cp -r /tmp/gfwlist_list.conf /etc/storage/gfwlist/gfwlist_list.conf
 mtd_storage.sh save >/dev/null 2>&1
-mkdir -p /tmp/dnsmasq.dom/
+mkdir -p /etc/storage/gfwlist/
 logger -st "gfwlist" "Update done"
 if [ $(nvram get ss_enable) = 1 ]; then
 logger -st "SS" "重启ShadowSocksR Plus+..."
@@ -20,3 +19,4 @@ fi
 else
 logger -st "gfwlist" "列表下载失败,请重试！"
 fi
+rm -f /tmp/gfwlist_list.conf
