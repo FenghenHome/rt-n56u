f11206a2fc6cf22f35e79bca51029d8813b8e9ff
FenghenHome <FenghenHome@users.noreply.github.com>
2021-03-18 12:56:19 +0800

    Update genv2config.lua

--- trunk/user/shadowsocks/ss/genv2config.lua
+++ trunk/user/shadowsocks/ss/genv2config.lua
@@ -8,8 +8,8 @@ local servertmp = ssrindext:read("*all")
 local server = cjson.decode(servertmp)
 local v2ray = {
 log = {
-	access = "/tmp/access.log",
-	error = "/tmp/error.log",
+	-- access = "/tmp/access.log",
+	-- error = "/tmp/error.log",
 	loglevel = "warning"
 },
 	-- 传入连接
