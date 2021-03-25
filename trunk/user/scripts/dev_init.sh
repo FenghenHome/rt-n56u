87fd9e0a22b7e54564b5f7cebcb7214c7b1979eb
FenghenHome <s.sunfeihu@gmail.com>
2021-03-25 13:58:51 +0800

    Update ca-certificates.crt
    Signed-off-by: FenghenHome <s.sunfeihu@gmail.com>

--- trunk/user/scripts/dev_init.sh
+++ trunk/user/scripts/dev_init.sh
@@ -49,7 +49,7 @@ mkdir -p -m 777 /tmp/rc_action_incomplete
 mkdir -p -m 700 /home/root
 mkdir -p -m 700 /home/root/.ssh
 mkdir -p -m 755 /etc/storage
-mkdir -p -m 755 /etc/ssl
+mkdir -p -m 755 /etc/ssl/certs
 mkdir -p -m 755 /etc/Wireless
 mkdir -p -m 750 /etc/Wireless/RT2860
 mkdir -p -m 750 /etc/Wireless/iNIC
@@ -65,6 +65,8 @@ fi
 
 if [ -f /etc_ro/ca-certificates.crt ]; then
 	ln -sf /etc_ro/ca-certificates.crt /etc/ssl/cert.pem
+	ln -sf /etc_ro/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
+
 fi
 # create symlinks
 ln -sf /home/root /home/admin
