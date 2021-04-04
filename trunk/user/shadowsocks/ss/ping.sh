b57b0645e6aa5f155726af04e7a83ca56468eb8c
FenghenHome <FenghenHome@users.noreply.github.com>
2021-04-04 17:42:24 +0800

    Update ping.sh

--- trunk/user/shadowsocks/ss/ping.sh
+++ trunk/user/shadowsocks/ss/ping.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 server_address=`lua /etc_ro/ss/getconfig.lua $1`
 server_port=`lua /etc_ro/ss/getportconfig.lua $1`
-ping_text=`ping -4 $server_address -c 5 -w 5 -q`
-ping_time=`echo $ping_text | awk -F '/' '{print $4}'`
+ping_text=`tcping -q -c 1 -i 1 -t 2 -p $server_port $server_address`
+ping_time=`echo $ping_text | grep -o 'time=[0-9]*' | awk -F '=' '{print $2}'`
 if [ -z "$ping_time" ]; then
 	ping_time="failed"
 else
