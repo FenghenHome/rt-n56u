6fa6687d5f2a15cdfee38e6622e1739ed2cfd390
FenghenHome <FenghenHome@users.noreply.github.com>
2021-04-15 16:33:23 +0800

    Update genv2config.lua

--- trunk/user/shadowsocks/ss/genv2config.lua
+++ trunk/user/shadowsocks/ss/genv2config.lua
@@ -6,7 +6,7 @@ local socks_port = arg[4] or "0"
 local ssrindext = io.popen("dbus get ssconf_basic_json_" .. server_section)
 local servertmp = ssrindext:read("*all")
 local server = cjson.decode(servertmp)
-local v2ray = {}
+local Xray = {}
 
 local log_settings = {
 	access = "/tmp/access.log",
@@ -18,6 +18,7 @@ local log_settings = {
 local inbounds_settings = {}
 
 	local inbounds_localin = (local_port ~= "0") and {
+		-- listening
 		tag = "local-in",
 		port = tonumber(local_port),
 		protocol = "dokodemo-door",
@@ -26,6 +27,7 @@ local inbounds_settings = {}
 	} or nil
 
 	local inbounds_socksin = (proto:find("tcp") and socks_port ~= "0") and {
+		-- socks
 		tag = "socks-in",
 		protocol = "socks",
 		port = tonumber(socks_port),
@@ -73,7 +75,7 @@ local routing_rules_settings = {}
     		"dns-in"
     	},
     	outboundTag = "dns-out"
-    }
+    	}
     	
 
 table.insert(routing_rules_settings,routing_rules_localin)
@@ -116,15 +118,24 @@ local outbounds_settings = {}
 		-- 底层传输配置
 		streamSettings = {
 			network = server.transport or "tcp",
-			security = (server.tls == '1') and "tls" or nil,
-			tlsSettings = (server.tls == '1' and (server.insecure == "1" or server.tls_host)) and {
+			security = (server.xtls == '1') and "xtls" or (server.tls == '1') and "tls" or nil,
+			tlsSettings = (server.tls == '1' and (server.insecure == "1" or server.tls_host or server.fingerprint)) and {
+				-- tls
+				fingerprint = server.fingerprint,
+				allowInsecure = (server.insecure == "1") and true or nil,
+				serverName = server.tls_host
+			} or nil,
+			xtlsSettings = (server.xtls == '1' and (server.insecure == "1" or server.tls_host)) and {
+				-- xtls
 				allowInsecure = (server.insecure == "1") and true or nil,
 				serverName = server.tls_host
 			} or nil,
 			tcpSettings = (server.transport == "tcp" and server.tcp_guise == "http") and {
+				-- tcp
 				header = {
 					type = server.tcp_guise,
 					request = {
+						-- request
 						path = {server.http_path} or {"/"},
 						headers = {Host = {server.http_host} or {}}
 					}
@@ -141,22 +152,27 @@ local outbounds_settings = {}
 				header = {type = server.kcp_guise}
 			} or nil,
 			wsSettings = (server.transport == "ws") and (server.ws_path or server.ws_host or server.tls_host) and {
+				-- ws
 				path = server.ws_path,
 				headers = (server.ws_host or server.tls_host) and {
+					-- headers
 					Host = server.ws_host or server.tls_host
 				} or nil
 			} or nil,
 			httpSettings = (server.transport == "h2") and {
+				-- h2
 				path = server.h2_path or "",
 				host = {server.h2_host} or nil
 			} or nil,
 			quicSettings = (server.transport == "quic") and {
+				-- quic
 				security = server.quic_security,
 				key = server.quic_key,
 				header = {type = server.quic_guise}
 			} or nil
 		},
-		mux = (server.mux == "1") and {
+		mux = (server.mux == "1" and server.xtls ~= "1") and {
+			-- mux
 			enabled = true,
 			concurrency = tonumber(server.concurrency)
 		} or nil
@@ -165,9 +181,9 @@ local outbounds_settings = {}
 table.insert(outbounds_settings,outbounds_dnsout)
 table.insert(outbounds_settings,outbounds_proxyout)
 
-v2ray["log"] = log_settings
-v2ray["inbounds"] = inbounds_settings
-v2ray["routing"] = routing_settings
-v2ray["outbounds"] = outbounds_settings
+Xray["log"] = log_settings
+Xray["inbounds"] = inbounds_settings
+Xray["routing"] = routing_settings
+Xray["outbounds"] = outbounds_settings
 
-print(cjson.encode(v2ray))
+print(cjson.stringify(Xray, 1))
