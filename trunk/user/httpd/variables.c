92acd65a59c19c5ddab968c05a73d1c3261a667f
FenghenHome <FenghenHome@users.noreply.github.com>
2021-04-17 14:45:41 +0800

    Update variables.c

--- trunk/user/httpd/variables.c
+++ trunk/user/httpd/variables.c
@@ -148,6 +148,7 @@
 			{"v2_quic_key_x", "24", NULL, FALSE},
 			{"v2_quic_security_x", "24", NULL, FALSE},
 			{"v2_tls_x", "24", NULL, FALSE},
+			{"v2_v2ray_protocol", "24", NULL, FALSE},
 			{"tj_tls_host_x", "24", NULL, FALSE},
 			{"switch_enable_x", "24", NULL, FALSE},
 			{0,0,0,0}
